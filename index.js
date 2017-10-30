2b42d16c0fc6963579e4fb8611416ffd44eb63c7
SammyHam <32743644+SammyHam@users.noreply.github.com>
2017-10-30 23:31:21 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+var leboncoin = require('./leboncoin');
+
+leboncoin.search({ category:leboncoin.CATEGORIES.LOCATION }).then( result => {
+
+    console.log( result );
+})
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const restService = express();
+
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+restService.use(bodyParser.json());
+
+restService.post('/webhook', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Oups quleques problèmes de connexion, peux-tu répéter s'il te plaît ?"
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+});
+
+restService.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
