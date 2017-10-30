588f54fe0e016aa2e89df09e667c9a84cdd99363
SammyHam <32743644+SammyHam@users.noreply.github.com>
2017-10-30 23:19:17 +0100

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,37 @@
+/*
+ * Contants used by leboncoin.
+ * We use theres constants to make search on leboncoin.
+ */
+
+module.exports = {
+    
+    CATEGORIES : {
+
+        LOCATION          : 'locations',
+        VENTE_IMMOBILIERE : 'ventes_immobilieres',
+        COLOCATION        : 'colocations',
+        BUREAU_COMMERCE   : 'bureaux_commerces'
+    },
+
+    TYPE_LOCATION : {
+
+        MAISON:1,
+        APPARTEMENT:2,
+        TERRAIN:3,
+        PARKING:4,
+        AUTRE:5
+    },
+
+    TYPE_VENTES_IMMOBILIERES : {
+
+        MAISON:1,
+        APPARTEMENT:2,
+        TERRAIN:3,
+        PARKING:4,
+        AUTRE:5
+    },
+
+    PARTICULIERS: 'p',
+    PROFESSIONNELS: 'c'
+}
+// TODO ajouter toutes les categories.
